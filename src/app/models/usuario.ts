faba805de5c89ea497199de04cef76cb7153f62e
Albert Morenilla <48762638+PrimalPres@users.noreply.github.com>
2021-01-17 23:41:08 +0100

    Modelo usuario creado

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+    id: number;
+    password: string;
+    email: string;
+    nombre: string;
+    apellidos: string;
+    telefono: string;
+    direccion: string;
+    nivel: number;
+}
