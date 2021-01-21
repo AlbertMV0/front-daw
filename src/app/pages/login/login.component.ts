cf5db34d01623da9b4b334afc54d7a61e38cee03
Albert Morenilla <48762638+PrimalPres@users.noreply.github.com>
2021-01-21 18:07:03 +0100

    recibir json de la api

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { UserService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -12,27 +13,32 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private form_builder: FormBuilder,private http: HttpClient) { }
+  constructor(private form_builder: FormBuilder, private http: HttpClient,private userService:UserService ) { }
 
-ngOnInit(){
-  //inicializamos la variable form
- this.loginForm= this.form_builder.group({
-email:[null,[Validators.email, Validators.required]],//[null,Validators.compose([Validators.email,Validators.required])]
-password:[null,Validators.required]
+  ngOnInit() {
+    //inicializamos la variable form
+    this.loginForm = this.form_builder.group({
+      email: [null, [Validators.email, Validators.required]],//[null,Validators.compose([Validators.email,Validators.required])]
+      password: [null, Validators.required]
 
-  });
-}
-public enviar():void{
-  
-  console.log(this.loginForm.getRawValue());
-  this.http.get('http://localhost:8000/api/usuarios').subscribe(
-      result=>{console.log("Correcto")
-      console.log(result);
+    });
+  }
+  public enviar(): void {
+    console.log(this.loginForm.getRawValue());
+    if (this.loginForm.valid) {
+    
+      this.userService.getUsuarios().subscribe(
+        (usuario:any)  =>
+        {
+          console.log("Correcto");
+          console.log(usuario);
 
-  },
-     error => {console.log("Error");
-     console.log(error);
-     }
-  );
-}
+        },
+        error => {
+          console.log("Error");
+          console.log(error);
+        }
+      );
+    }
+  }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient,HttpHeaders,HttpParams } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
 import { Observable, throwError } from 'rxjs';
 import { Usuario } from "../models/usuario";
 import { pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-@Injectable() export class UserService {
+@Injectable(
+{providedIn:'root'
 
-  constructor(private http: HttpClient) {}
+  /*constructor(private http: HttpClient) {}
 
   getUsuario():Observable <any> {
     return this.http.get('/api/usuario');
@@ -15,9 +17,36 @@ import { map } from 'rxjs/operators';
 
   getAllProfe():Observable <any> {
     return this.http.get('/api/usuario');
-  }
-}
+  }*/
+})
 
-export class UsuarioService {
-  constructor() { }
+export class UserService {
+  constructor(private http:HttpClient) { }
+
+  /*login(values): Observable<any> {
+    return this.http.post(environment.url + '/login', values).pipe(
+      map((loggedUser: IUsuario) => {
+        sessionStorage.setItem('token', loggedUser.sessionId);
+        return this.loggedUser = loggedUser;
+      })
+    );
+  }
+  */
+  getUsuarios(): Observable<any> {
+    /*const httpOptions = {
+      headers: new HttpHeaders({
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-Requested-With': 'XMLHttpRequest',
+      }),
+    };*/
+    return this.http.get('http://localhost:8000/api'+ '/usuarios')
+    .pipe(map((results) => results));
+  }
+
+  login(values): Observable<any> {
+    
+    return this.http.get('http://localhost:8000/oauth'+ '/token')
+    .pipe(map((results) => results));
+  }
 }
